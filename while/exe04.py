ab96e6da9b03701bc963b7d2a94b2e321b1015b6
Gabriel Chagas Lage <107369547+gacl13@users.noreply.github.com>
2022-06-14 21:13:11 -0300

    Update exe04.py

--- while/exe04.py
+++ while/exe04.py
@@ -5,6 +5,7 @@ from random import randint
 vida_do_monstro = 20
 vida = 13
 
+
 #historinha
 print ("Bem vindo aventureiro ! Você está prestes a entrar no sombrio mundo da programação")
 time.sleep(1.5)
@@ -15,6 +16,12 @@ time.sleep(1.5)
 print ("\nEstamos entrando no mundo de Programaçãolandia")
 time.sleep(1.5)
 personagem = input("\nAgora escolha seu personagem, um mago fodastico tipo Gandalf e Dumbledore ou um guerreiro tipo Ragnar ou Rei Arthur com uma espada pegando fogo \n-> ")
+print (f"\nAgora você está entrando nas masmoras, e se depara com o monstro C, um dos mais antigos monstros, ele tem {vida_do_monstro} de vida. Você como {personagem} faz o que?\n")
+
+
+
+
+
 
 #codigo
 while True:
@@ -22,102 +29,132 @@ while True:
         continue
     break
          
-
+while True:
    #guerreiro
-if personagem == "Guerreiro":
+    if personagem == "Guerreiro":
 
 
-     personagem == "Guerreiro"    
-     print (f"\nAgora você está entrando nas masmoras, e se depara com o monstro C, um dos mais antigos monstros, ele tem {vida_do_monstro} de vida. Você como {personagem} faz o que?\n")
-     time.sleep(1.5)
-     acao = input("Escolha sua acao: \n Atacar \n Defender \n Curar \n Descansar: ")
-     if  not "Atacar" in acao or not "Defender" in acao or not "Curar" in acao or not "Descansar" in acao:
-       atacar = randint(3, 10)
-       ataque_monstro = 3
-       vida = 13
-       cura = vida + 1
-       descansar = randint(1,5)
-       poder = randint(5, 10)
+        personagem == "Guerreiro"    
+        time.sleep(1.5)
+        acao = input("Escolha sua acao: \n Atacar \n Defender \n Curar \n Descansar: ")
+        if  not "Atacar" in acao or not "Defender" in acao or not "Curar" in acao or not "Descansar" in acao:
+         atacar = randint(3, 10)
+        ataque_monstro = 3
+        vida = 13
+        cura = vida + 1
+        descansar = randint(1,5)
+        poder = randint(5, 10)
 
-       if acao == "Atacar":
-        poder -= 2
-        vida -= ataque_monstro
-        vida_do_monstro -= atacar
-        print(f"Sua vida: {vida}")
-        print(f"Dano do ataque foi: {atacar}")
-        print(f"A vida do monstro: {vida_do_monstro}")
-        print(f"Ataque do monstro foi: {ataque_monstro} ")
-    
-       elif acao == "Defender":
-        poder -= 1
-        print(f"Sua vida: {vida}")
-        print(f"A vida do monstro: {vida_do_monstro}")
-    
-       elif acao == "Curar":
-        vida += cura
-        vida -= ataque_monstro
-        print(f"Sua vida: {vida}")
-        print(f"Dano do ataque foi: {atacar}")
-        print(f"Ataque do monstro foi: {ataque_monstro} ")
-    
-       elif acao == "Descansar":
-        poder += descansar
-        vida -= ataque_monstro
-    
-     else:
-        print("Por favor digite novamente")   
-       
+        if acao == "Atacar":
+            poder -= 2
+            vida -= ataque_monstro
+            vida_do_monstro -= atacar
+            print(f"Sua vida: {vida}")
+            print(f"Dano do ataque foi: {atacar}")
+            print(f"A vida do monstro: {vida_do_monstro}")
+            print(f"Ataque do monstro foi: {ataque_monstro} ")
+        
+        elif acao == "Defender":
+            poder -= 1
+            print(f"Sua vida: {vida}")
+            print(f"A vida do monstro: {vida_do_monstro}")
+        
+        elif acao == "Curar":
+            vida += cura
+            vida -= ataque_monstro
+            print(f"Sua vida: {vida}")
+            print(f"Dano do ataque foi: {atacar}")
+            print(f"Ataque do monstro foi: {ataque_monstro} ")
+        
+        elif acao == "Descansar":
+            poder += descansar
+            vida -= ataque_monstro
+        
+        else:
+            print("Por favor digite novamente")   
+        
 
-#mago
-else:
-    personagem == "Mago"    
-    print (f"\nAgora você está entrando nas masmoras, e se depara com o monstro C, um dos mais antigos monstros, ele tem {vida_do_monstro} de vida. Você como {personagem} faz o que?\n")
-    time.sleep(1.5)
-
-    acao = input("Escolha sua acao: \n Atacar \n Defender \n Curar \n Descansar: ")
-    if  not "Atacar" in acao or not "Defender" in acao or not "Curar" in acao or not "Descansar" in acao:
-       atacar = randint(0, 8)
-       ataque_monstro = 3
-       vida = 13
-       cura = vida + 2
-       descansar = randint(1,5)
-       poder = randint(5, 10)
-
-       if acao == "Atacar":
-        poder -= 2
-        vida -= ataque_monstro
-        vida_do_monstro -= atacar
-        print(f"Sua vida: {vida}")
-        print(f"Dano do ataque foi: {atacar}")
-        print(f"A vida do monstro: {vida_do_monstro}")
-        print(f"Ataque do monstro foi: {ataque_monstro} ")
-    
-       elif acao == "Defender":
-        poder -= 1
-        print(f"Sua vida: {vida}")
-        print(f"A vida do monstro: {vida_do_monstro}")
-    
-       elif acao == "Curar":
-        vida += cura
-        vida -= ataque_monstro
-        print(f"Sua vida: {vida}")
-        print(f"Dano do ataque foi: {atacar}")
-        print(f"Ataque do monstro foi: {ataque_monstro} ")
-    
-       elif acao == "Descansar":
-        poder += descansar
-        vida -= ataque_monstro
-    
+    #mago
     else:
-        print("Por favor digite novamente")   
+        personagem == "Mago"    
+      
+        time.sleep(1.5)
 
-if vida == 0:
-    "Você morreu, gamer over"
-    exit()
-if vida_do_monstro == 0:
-    "Parabens! Você ganou o monstro morreu"
-    exit ()        
+        acao = input("Escolha sua acao: \n Atacar \n Defender \n Curar \n Descansar: ")
+        if  not "Atacar" in acao or not "Defender" in acao or not "Curar" in acao or not "Descansar" in acao:
+         atacar = randint(0, 8)
+        ataque_monstro = 3
+        vida = 13
+        cura = vida + 2
+        descansar = randint(1,5)
+        poder = randint(5, 10)
 
+        if acao == "Atacar":
+            poder -= 2
+            vida -= ataque_monstro
+            vida_do_monstro -= atacar
+            print(f"Sua vida: {vida}")
+            print(f"Dano do ataque foi: {atacar}")
+            print(f"A vida do monstro: {vida_do_monstro}")
+            print(f"Ataque do monstro foi: {ataque_monstro} ")
+        
+        elif acao == "Defender":
+            poder -= 1
+            print(f"Sua vida: {vida}")
+            print(f"A vida do monstro: {vida_do_monstro}")
+        
+        elif acao == "Curar":
+            vida += cura
+            vida -= ataque_monstro
+            print(f"Sua vida: {vida}")
+            print(f"Dano do ataque foi: {atacar}")
+            print(f"Ataque do monstro foi: {ataque_monstro} ")
+        
+        elif acao == "Descansar":
+            poder += descansar
+            vida -= ataque_monstro
+        
+        else:
+            print("Por favor digite novamente")   
 
-  
-     
+        if vida <= 0:
+            print ("Você morreu, gamer over")
+            while True:
+                resposta = input("Você morreu, quer jogar novamente?")
+
+                if resposta == "Sim":
+                    break
+                elif resposta == "Nao":
+                    break
+                else: 
+                    continue
+
+            print ("Que pena! Só os fortes ganham")
+
+            if resposta == "Sim":
+                vida_do_monstro = 20
+                vida = 13
+                continue
+            else:
+                break
+
+        if vida_do_monstro <= 0:
+            while True:
+                resposta = input("Você matou o monstro, quer enfrentar outro?")
+
+                if resposta == "Sim":
+                    break
+                elif resposta == "Nao":
+                    break
+                else: 
+                    continue
+
+            print ("Parabens! Você ganhou o monstro morreu")
+
+            if resposta == "Sim":
+                vida_do_monstro = 10
+                continue
+            else:
+                break
+
+             
